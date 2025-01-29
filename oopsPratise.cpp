cea7f1e54326b4180a113dd60534901ae9185b44
asadullah <asadullahjamilchaudhry@gmail.com>
2025-01-29 21:18:23 +0500

    User-defined copy constructor in oops.

--- oopsPratise.cpp
+++ oopsPratise.cpp
@@ -7,15 +7,25 @@ private:
     double salary;
 
 public:
+    string name;
+    string dept;
+    string programmingLanguage;
+
     Teacher(string name, string dept, string programmingLanguage)
     {
         this->name = name;
         this->dept = dept;
         this->programmingLanguage = programmingLanguage;
     }
-    string name;
-    string dept;
-    string programmingLanguage;
+
+    Teacher(Teacher &orgObj)
+    {
+        cout << "I am Copy Constructor!" << endl;
+        this->name = orgObj.name;
+        this->dept = orgObj.dept;
+        this->programmingLanguage = orgObj.programmingLanguage;
+        this->salary = orgObj.salary;
+    }
 
     void setSal(double salary)
     {
@@ -25,16 +35,25 @@ public:
     {
         cout << "Salary:" << salary << endl;
     }
+    void getInfo()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Departement: " << dept << endl;
+        cout << "Programming Language: " << programmingLanguage << endl;
+    }
 };
 
 int main()
 {
-    Teacher t1("Jamil", "computerScience", "c++", 345000);
-    cout << "Name: " << t1.name << endl;
-    cout << "Departement: " << t1.dept << endl;
-    cout << "Programming Language: " << t1.programmingLanguage << endl;
+    Teacher t1("Jamil", "computerScience", "c++");
+
+    t1.getInfo();
     t1.setSal(50000);
     t1.getSal();
+
+    cout << "This is for 2nd Object:" << endl;
+    Teacher t2(t1);
+    t2.getInfo();
 
     return 0;
 }
